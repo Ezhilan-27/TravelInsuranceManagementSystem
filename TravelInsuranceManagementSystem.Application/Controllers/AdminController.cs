11e6c2bb10528daa520fe1b4703721f23a2524dc
Shamim <2463419@cognizant.com>
2025-12-25 13:39:29 +0530

    Resolved conflicts by accepting incoming changes

---
+++ TravelInsuranceManagementSystem.Application/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TravelInsuranceManagementSystem.Application.Controllers
+{
+    public class AdminController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public IActionResult Dashboard() => View();
+        public IActionResult Policies() => View();
+        public IActionResult Claims() => View();
+        public IActionResult Payments() => View();
+
+    }
+}
---
+++ TravelInsuranceManagementSystem.Application/wwwroot/js/admin.js
@@ -0,0 +1,10 @@
+﻿
+(function () {
+    const path = location.pathname.toLowerCase();
+    document.querySelectorAll('.nav-item').forEach(a => {
+        const href = a.getAttribute('href')?.toLowerCase() || '';
+        if (path.includes(href.toLowerCase())) {
+            a.style.background = 'var(--gray)';
+        }
+    });
+})();
